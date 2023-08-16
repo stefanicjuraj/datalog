6d4d717f689de4674e00c67f666b360631ec3a7d
stefanicjuraj <jurajstefanic@outlook.com>
2023-08-16 17:39:40 +0200

    feat(app): add route to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/404";
+import Conferences from "./pages/Conferences";
+import Internships from "./pages/Internships";
+import Education from "./pages/Education";
+import Certificates from "./pages/Certificates";
+import Contribute from "./pages/Contribute";
 
 function App() {
     return (
         <Router>
             <Sidebar />
             <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="/conferences" element={<Conferences />} />
+                <Route path="/internships" element={<Internships />} />
+                <Route path="/education" element={<Education />} />
+                <Route path="/certificates" element={<Certificates />} />
+                <Route path="/contribute" element={<Contribute />} />
             </Routes>
         </Router >
     );
