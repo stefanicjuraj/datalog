b792bf7441ddc21b1ed8c751025fb1b7a0725af8
stefanicjuraj <jurajstefanic@outlook.com>
2023-08-18 02:48:10 +0200

    feat(app): add route to about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import PageNotFound from "./pages/404";
 import Conferences from "./pages/Conferences";
 import Internships from "./pages/Internships";
@@ -15,6 +16,7 @@ function App() {
             <Sidebar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/" element={<About />} />
                 <Route path="*" element={<PageNotFound />} />
                 <Route path="/conferences" element={<Conferences />} />
                 <Route path="/internships" element={<Internships />} />
