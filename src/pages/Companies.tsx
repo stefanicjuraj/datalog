49a5fd7acb97ab688de55b76ea9b0b6342ad4540
stefanicjuraj <jurajstefanic@outlook.com>
2023-09-03 15:51:25 +0200

    fix(companies): modify headings

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -14,29 +14,29 @@ function Companies() {
                             </h1>
                             {/* header tags */}
                             <div className="mt-4 mb-4 text-white flex">
-                                <a className="text-md font-bold text-center rounded-full inline-flex items-center">
+                                <p className="text-md font-bold text-center rounded-full inline-flex items-center">
                                     {/* animation */}
                                     <span className="text-normal flex items-center">
                                         <span className="w-2 h-2 mr-2 bg-white animate-pulse rounded-full flex flex-shrink-0"></span>
                                     </span>
                                     {/* header tag text */}
                                     In development
-                                </a>
+                                </p>
                                 {/* header tags divider */}
                                 <a className="ml-3 mr-3 text-md text-center rounded-full inline-flex items-center">
                                     <span className="text-normal flex items-center">|</span>
                                 </a>
                                 {/* header tag  */}
-                                <a className="text-white text-md text-center inline-flex items-center">
+                                <h1 className="text-white text-md text-center inline-flex items-center">
                                     {/* pin location icon */}
                                     <img src={pin} className="h-5 w-5 mr-1" />
                                     Croatia
-                                </a>
+                                </h1>
                             </div>
                             {/* header description */}
-                            <p className="w-full sm:w-3/4 text-lg mt-2 text-white">
+                            <h1 className="w-full sm:w-3/4 text-lg mt-2 text-white">
                                 A curated datalog of IT (ICT) <span className="bg-[#333] px-1 py-0.3 rounded-lg">companies</span>, <span className="bg-[#333] px-1 py-0.3 rounded-lg">agencies</span>,{" "} and <span className="bg-[#333] px-1 py-0.3 rounded-lg">consultancies</span> conducting business operations, providing services, or offering consultancy in Croatia.
-                            </p>
+                            </h1>
                         </div>
                     </div>
                 </div>
