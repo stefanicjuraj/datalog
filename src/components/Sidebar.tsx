1155b3df1b366c03ed2bcb88f6dbc26705ae5295
stefanicjuraj <jurajstefanic@outlook.com>
2023-09-08 23:05:59 +0200

    feat(sidebar): hide sidebar & add sidebar toggle

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -89,7 +89,7 @@ const Sidebar: React.FC = () => {
                     <div className="flex items-center justify-between">
                         <div className="flex items-center justify-start">
                             {/* navbar toggle menu */}
-                            <a aria-controls="logo-sidebar" type="button" onClick={toggleSidebar} className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-[#333] focus:outline-none focus:ring-2 focus:ring-[#333] cursor-pointer">
+                            <a aria-controls="logo-sidebar" type="button" onClick={toggleSidebar} className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-[#333] focus:outline-none focus:ring-2 focus:ring-[#333] cursor-pointer">
                                 <img src={menu} className="h-6 w-6" alt="menu" />
                             </a>
                             {/* favicon */}
@@ -111,7 +111,7 @@ const Sidebar: React.FC = () => {
                 </div>
             </nav>
             {/* sidebar */}
-            <aside id="logo-sidebar" className={`transition-transform left-0 top-0 -translate-x-full fixed top-0 left-0 z-40 w-56 h-screen pt-36 transition-transform ${sidebarOpen ? "translate-x-0" : "-translate-x-full"} bg-[#F9FAFB] border-r border-[#EEE] sm:translate-x-0`} aria-label="Sidebar">
+            <aside id="logo-sidebar" className={`transition-transform left-0 top-0 fixed top-0 left-0 z-40 w-56 h-screen pt-36 transition-transform ${sidebarOpen ? "md:-translate-x-0" : "md:-translate-x-full"} bg-[#F9FAFB] border-r border-[#EEE] ${sidebarOpen ? "-translate-x-0" : "-translate-x-full"}`} aria-label="Sidebar">
                 <div className="h-full px-3 pb-4 overflow-y-auto bg-[#F9FAFB]">
                     <ul className="space-y-3 font-medium">
                         <li>
