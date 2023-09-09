0238f2c84e113f11bbbda3d9196843282e76f046
stefanicjuraj <jurajstefanic@outlook.com>
2023-09-09 23:30:04 +0200

    feat(conferences): add table functionality

--- src/pages/Conferences.tsx
+++ src/pages/Conferences.tsx
@@ -1,9 +1,165 @@
-// import icons
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+// import icons
 import pin from '../assets/icons/pin.svg';
 import search from '../assets/icons/search.svg';
+import loading from '../assets/icons/loading.svg';
+import chevronUp from '../assets/icons/chevron-up-light.svg';
+import chevronDown from '../assets/icons/chevron-down-light.svg';
+import ticket from '../assets/icons/ticket.svg';
+import location from '../assets/icons/pin-dark.svg';
+
+interface Conference {
+    "Conference": string;
+    "Website": string;
+    "Topic": string[];
+    "Organizer": string;
+    "Contact": string;
+    "Location": string;
+    "Start date": string;
+    "End date": string;
+    "Ticket": string;
+}
+
+function ConferencesTable({ conference }: { conference: Conference }) {
+    return (
+        <tr className="bg-[#FFF] hover:bg-[#fff] border-b">
+            {/* conference */}
+            <th scope="row" className="pl-6 py-5 text-lg text-black font-bold whitespace-nowrap">
+                {conference["Conference"]}
+                <br />
+                {/* website */}
+                <a href={conference["Website"]} className="text-base text-[#aaa] font-normal hover:underline" target="_blank" rel="noopener noreferrer">
+                    {conference["Website"]}
+                </a>
+            </th>
+            {/* topic */}
+            <td className="sm:pl-0 pl-4">
+                {conference["Topic"] && conference["Topic"].map((topic, index) => (
+                    <a key={index} className="mr-1 px-3 py-2 text-sm text-black font-medium bg-[#eee] rounded-xl text-center items-center" target="_blank" rel="noopener noreferrer">
+                        {/* topic data */}
+                        {topic}
+                    </a>
+                ))}
+            </td>
+            {/* organizer */}
+            <td className="sm:pl-0 pl-4">
+                <a href={conference["Contact"]} className="px-4 py-3 mr-0 mb-2 text-black text-md text-left bg-white hover:ring-1 hover:shadow-sm hover:ring-[#eee] focus:ring-2 focus:outline-none font-bold rounded-xl inline-flex items-center underline" target="_blank" rel="noopener noreferrer">
+                    {/* organizer data */}
+                    {conference["Organizer"]}
+                </a>
+            </td>
+            {/* location */}
+            <td className="sm:pl-0 pl-4">
+                <a className="px-3.5 py-2.5 inline-flex items-center" rel="noopener noreferrer">
+                    {/* location icon */}
+                    <img src={location} className="h-6 w-6 mr-1" alt="contact" />
+                    {/* location data */}
+                    {conference["Location"]}
+                </a>
+            </td>
+            {/* start & end date */}
+            <td className="sm:pl-8 pl-8">
+                <p className="inline-flex items-center" rel="noopener noreferrer">
+                    {/* careers */}
+                    {conference["Start date"]}
+                    <br />
+                    {conference["End date"]}
+                </p>
+            </td>
+            {/* ticket */}
+            <td className="sm:pl-4 pl-6">
+                <a href={conference["Ticket"]} className="px-3.5 py-2.5 mr-0 mb-2 bg-[#eee] hover:ring-1 hover:shadow-sm hover:ring-gray-200 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-xl inline-flex items-center" target="_blank" rel="noopener noreferrer">
+                    {/* ticket icon */}
+                    <img src={ticket} className="h-7 w-7" alt="ticket" />
+                </a>
+            </td>
+        </tr>
+    );
+}
 
 function Conferences() {
+    const [jsonData, setJsonData] = useState<Conference[]>([]);
+    const [filteredData, setFilteredData] = useState<Conference[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+    const [isLoading, setIsLoading] = useState(true);
+
+    // fetch data
+    useEffect(() => {
+        setIsLoading(true);
+        fetch("")
+            .then((response) => response.json())
+            .then((jsonDataResponse) => {
+                setJsonData(jsonDataResponse);
+                setFilteredData(jsonDataResponse);
+            })
+            .catch((error) => console.error("Error fetching data: ", error))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    // filter data by company name
+    const filterData = (searchTerm: string) => {
+        const filteredData = jsonData.filter((conference) =>
+            conference["Conference"].toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredData(filteredData);
+    };
+
+    // sort data
+    const sortData = () => {
+        const sortedData = [...filteredData].sort((a, b) => {
+            const aValue = a["Conference"].toLowerCase();
+            const bValue = b["Conference"].toLowerCase();
+            return sortOrder === "asc" ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+        });
+        setFilteredData(sortedData);
+    };
+
+    // sort data order
+    useEffect(() => {
+        sortData();
+    }, [sortOrder]);
+
+    // sort by company column
+    const handleSort = () => {
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    };
+
+    // search input
+    const tableRef = useRef<HTMLTableElement>(null);
+
+    // handle search input change
+    const handleSearchInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const searchTerm = event.target.value.trim();
+        setSearchTerm(searchTerm);
+        filterData(searchTerm);
+
+        // search results
+        if (tableRef.current) {
+            tableRef.current.innerHTML =
+                filteredData.map((conference) => (
+                    <ConferencesTable
+                        key={conference["Conference"]}
+                        conference={conference}
+                    />
+                )).join("");
+        }
+    };
+
+    // table no results message
+    function NoResultsMessage() {
+        return (
+            <tr>
+                <td colSpan={6} className="text-center py-16 text-black">
+                    No results found.
+                </td>
+            </tr>
+        );
+    }
+
     return (
         <div className="pl-4 pr-4 pt-12">
             <div className="max-w-screen-xl xl:max-w-screen-2xl mx-auto">
@@ -52,10 +208,10 @@ function Conferences() {
                                     <img src={search} className="h-4 w-4" alt="Search Icon" />
                                 </div>
                                 {/* search input */}
-                                <input type="email" id="searchInput" className="block w-full p-4 pl-10 text-base text-black border border-gray-300 rounded-2xl focus:ring-black focus:border-black" placeholder="Search by conference" />
+                                <input value={searchTerm} onChange={handleSearchInputChange} type="email" id="searchInput" className="block w-full p-4 pl-10 text-base text-black border border-gray-300 rounded-2xl focus:ring-black focus:border-black" placeholder="Search by conference" />
                                 <p id="searchResults" className="text-black absolute right-2 bottom-2 bg-[#eee] focus:ring-4 focus:outline-none rounded-xl text-base px-5 py-2">
                                     {/* search filtered data - results count */}
-                                    results
+                                    {filteredData.length} results
                                 </p>
                             </div>
                         </form>
@@ -79,16 +235,17 @@ function Conferences() {
                             <thead className="text-xl text-white animation glow delay-2">
                                 <tr className="bg-[#222]">
                                     {/* conference */}
-                                    <th className="sm:px-6 px-6 py-7 whitespace-nowrap cursor-pointer rounded-l-xl">
-                                        <h1>Conference</h1>
+                                    <th onClick={handleSort} className="sm:px-6 px-6 py-7 whitespace-nowrap cursor-pointer rounded-l-xl">
+                                        <h1 className="inline-flex">Conference</h1>
+                                        {sortOrder === "asc" ? <img src={chevronUp} className="h-5 w-5 ml-1 inline-flex" alt="Up Arrow" /> : <img src={chevronDown} className="h-5 w-5 ml-1 inline-flex" alt="sort arrow" />}
                                     </th>
                                     {/* topics & departments */}
                                     <th className="sm:px-0 px-6 py-3 whitespace-nowrap">
-                                        <h1>Topics & Departments</h1>
+                                        <h1>Topics & Discussions</h1>
                                     </th>
                                     {/* host */}
                                     <th className="sm:px-3 px-6 py-3 whitespace-nowrap">
-                                        <h1>Host</h1>
+                                        <h1>Organizer</h1>
                                     </th>
                                     {/* location */}
                                     <th className="sm:px-3 px-6 py-3 whitespace-nowrap">
@@ -96,22 +253,35 @@ function Conferences() {
                                     </th>
                                     {/* date */}
                                     <th className="sm:px-3 px-6 py-3 whitespace-nowrap">
-                                        <h1>Date</h1>
+                                        <h1>Start / End date</h1>
                                     </th>
                                     {/* tickets */}
                                     <th className="sm:px-3 px-6 py-3 whitespace-nowrap rounded-r-xl">
-                                        <h1>Tickets</h1>
+                                        <h1>Ticket</h1>
                                     </th>
                                 </tr>
                             </thead>
                             {/* table body */}
                             <tbody id="companyTableBody" className="animation glow delay-3">
-                                <tr>
-                                    <td colSpan={5} className="text-center py-16">
-                                        <div className="flex items-center justify-center">
-                                        </div>
-                                    </td>
-                                </tr>
+                                {/* loading */}
+                                {isLoading ? (
+                                    <tr>
+                                        <td colSpan={6} className="text-center py-16">
+                                            <div className="flex items-center justify-center">
+                                                {/*  loading spinner icon */}
+                                                <img src={loading} className="h-10 w-10 animate-spin" alt="loading" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ) : (
+                                    filteredData.length === 0 ? (
+                                        <NoResultsMessage />
+                                    ) : (
+                                        filteredData.map((conference) => (
+                                            <ConferencesTable key={conference["Conference"]} conference={conference} />
+                                        ))
+                                    )
+                                )}
                             </tbody>
                         </table>
                     </div>
