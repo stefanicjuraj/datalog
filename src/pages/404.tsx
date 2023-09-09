9e942872eed19950a3ef5592517e908ffc22c503
stefanicjuraj <jurajstefanic@outlook.com>
2023-09-09 23:28:15 +0200

    style(404): center content

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import arrow from '../assets/icons/arrow-dark.svg';
 
 function PageNotFound() {
     return (
-        <div className="pl-4 pr-4 pt-12 sm:ml-56">
+        <div className="pl-4 pr-4 pt-12">
             <div className="max-w-screen-xl xl:max-w-screen-2xl mx-auto">
                 <div className="mt-24 sm:px-4 px-0 mx-auto max-w-screen-xl">
                     {/* header */}
