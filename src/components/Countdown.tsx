db8a65132728992d485469fd3dcb7fe3531d931d
stefanicjuraj <jurajstefanic@outlook.com>
2023-09-10 02:17:05 +0200

    feat(components): add countdown

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+interface CountdownProps {
+    startDate: string;
+}
+
+const Countdown: React.FC<CountdownProps> = ({ startDate }) => {
+    const [daysLeft, setDaysLeft] = useState<number | null>(null);
+
+    useEffect(() => {
+        const currentDate = new Date();
+        const conferenceDate = new Date(startDate + ', 2023');
+
+        if (currentDate > conferenceDate) {
+            setDaysLeft(-1);
+        } else {
+            const timeDifference = conferenceDate.getTime() - currentDate.getTime();
+            const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+            setDaysLeft(daysRemaining);
+        }
+    }, [startDate]);
+
+    return (
+        <p className="px-2.5 py-1.5 text-sm text-black font-bold bg-[#eee] rounded-full text-center items-center">
+            {daysLeft !== null ? (
+                daysLeft === -1 ? (
+                    '-'
+                ) : (
+                    <>
+                        {daysLeft} {daysLeft === 1 ? 'day' : 'days'}
+                    </>
+                )
+            ) : (
+                '...'
+            )}
+        </p>
+    );
+};
+
+export default Countdown;
