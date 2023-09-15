f3a1a411c0e7a3430e45c10de9ef25cdd7f7f481
stefanicjuraj <jurajstefanic@outlook.com>
2023-09-16 01:22:49 +0200

    chore(internships): modify table data column

--- src/pages/Internships.tsx
+++ src/pages/Internships.tsx
@@ -39,7 +39,7 @@ function InternshipsTable({ internship }: { internship: Internship }) {
             </td>
             {/* company */}
             <td className="sm:pl-0 pl-3">
-                <a className="px-4 py-3 mr-0 mb-2 text-black text-md text-left font-bold rounded-xl inline-flex items-center truncate" target="_blank" rel="noopener noreferrer">
+                <a href={internship["Website"]} className="px-4 py-3 mr-0 mb-2 text-black text-md text-left bg-white hover:ring-1 hover:shadow-sm hover:ring-[#eee] focus:ring-2 focus:outline-none font-bold rounded-xl inline-flex items-center underline" target="_blank" rel="noopener noreferrer">
                     {/* company data */}
                     {internship["Company"]}
                 </a>
