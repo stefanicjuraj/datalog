ba2f261cfda9b81f31345e148a2428f767f3a975
stefanicjuraj <jurajstefanic@outlook.com>
2023-09-24 16:03:21 +0200

    feat(countdown): add red icon for held events

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -22,12 +22,12 @@ const Countdown: React.FC<CountdownProps> = ({ startDate }) => {
 
     return (
         <p className="px-2.5 py-1.5 text-sm text-black font-bold bg-[#eee] rounded-full text-center items-center">
-            <p className="mr-1 w-2 h-2 animate-pulse bg-green-500 inline-flex rounded-full flex-shrink-0"></p>
             {daysLeft !== null ? (
                 daysLeft === -1 ? (
-                    '-'
+                    <p className="w-2 h-2 bg-red-500 inline-flex rounded-full flex-shrink-0"></p>
                 ) : (
                     <>
+                        <p className="mr-1 w-2 h-2 animate-pulse bg-green-500 inline-flex rounded-full flex-shrink-0"></p>
                         {daysLeft} {daysLeft === 1 ? 'day' : 'days'}
                     </>
                 )
